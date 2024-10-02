15fbe5102b62f0d1a82f5e7061121e30d8322ce6
Federico Di Iorio <federico_metallica@hotmail.com>
2024-10-02 06:24:20 -0300

    Se completaron los controller

---
+++ src/controller/cart.controller.js
@@ -0,0 +1,124 @@
+import CartService from '../repository/carts.repository.js';
+
+export default class Controller {
+
+    #cartService;
+
+    constructor() {
+        this.#cartService = new CartService();
+    };
+
+    async getCarts(req, res) {
+        try {
+            const carts = await this.#cartService.getCarts();
+
+            res.status(200).json(carts);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async getCartById(req, res) {
+        try {
+            const cartId = req.params.cid;
+            const cart = await this.#cartService.getCartById(cartId);
+
+            res.status(200).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async createCart(req, res) {
+        try {
+            const cart = await this.#cartService.addCart();
+            req.logger.info('Carrito creado de manera correcta');
+
+            res.status(201).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async addProductToCart(req, res) {
+        try {
+            const cartId = req.params.cid;
+            const productId = req.params.pid;
+            const user = req.user;
+            const cart = await this.#cartService.addProductToCart(productId, cartId, user);
+            req.logger.info('Producto agregado al carrito de manera correcta');
+
+            res.status(200).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async deleteProductFromCart(req, res) {
+        try {
+            const cartId = req.params.cid;
+            const productId = req.params.pid
+            const cart = await this.#cartService.deleteProductFromCart(productId, cartId);
+            req.logger.info('Producto eliminado del carrito.');
+
+            res.status(200).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async updateCart(req, res) {
+        try {
+            const cartId = req.params.cid;
+            const products = req.body;
+            const cart = await this.#cartService.updateCart(cartId, products);
+            req.logger.info('Se ha actualizado el carrito de manera correcta');
+
+            res.status(200).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async updateProductQuantity(req, res) {
+        try {
+            const cartId = req.params.cid;
+            const productId = req.params.pid;
+            const { quantity } = req.body;
+            const cart = await this.#cartService.updateProductQuantity(cartId, productId, quantity);
+            req.logger.info('Se ha actualizado la cantidad de manera correcta');
+
+            res.status(200).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async clearCart(req, res) {
+        try {
+            const cartId = req.params.cid;
+            const cart = await this.#cartService.clearCart(cartId);
+            req.logger.info('Se ha vaciado el carrito de manera correcta');
+
+            res.status(204).json(cart);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+};
---
+++ src/controller/products.controller.js
@@ -0,0 +1,99 @@
+import ProductService from '../repository/products.repository.js';
+
+export default class Controller {
+
+    #productService;
+
+    constructor() {
+        this.#productService = new ProductService();
+    };
+
+    async getPaginateProducts(req, res) {
+        try {
+            const page = req.query.page || 1;
+            const limit = req.query.limit || 10;
+            const sort = req.query.sort;
+            const category = req.query.category;
+            const availability = req.query.availability;
+
+            const products = await this.#productService.getPaginateProducts(page, limit, sort, category, availability);
+
+            res.status(200).json(products);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async getProducts(req, res) {
+        try {
+            const products = await this.#productService.getProducts();
+
+            res.status(200).json(products);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+        ;
+    }
+
+    async getProductById(req, res) {
+        try {
+            const productId = req.params.pid;
+            const product = await this.#productService.getProductById(productId);
+
+            res.status(200).json(product);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async addProduct(req, res) {
+        try {
+            const { title, description, price, code, stock, category } = req.body;
+            const owner = req.user.email;
+            const thumbnail = req.file;
+            const product = await this.#productService.addProduct({ title, description, price, thumbnail, code, stock, category, owner });
+            req.logger.info('Producto creado de manera correcta');
+
+            res.status(201).json(product);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async updateProduct(req, res) {
+        try {
+            const productId = req.params.pid;
+            const updatedProduct = await this.#productService.updateProduct(productId, req.body);
+            req.logger.info('Producto actualizado de manera correcta');
+
+            res.status(201).json(updatedProduct);
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+
+    async deleteProduct(req, res) {
+        try {
+            const productId = req.params.pid;
+            const user = req.user
+            await this.#productService.deleteProduct(productId, user);
+            req.logger.info('Producto eliminado de manera correcta')
+
+            res.status(204).json({ message: 'Producto eliminado' });
+
+        } catch (error) {
+            req.logger.error(error);
+            res.status(error.status).json({ error });
+        };
+    };
+};
